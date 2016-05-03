e2028a6849e53713efbfa16ab705b286d91fa020
tatsuya hirose <tatuyahirose0805@gmail.com>
2016-05-03 18:53:49 +0900

    画像アップロード画面css上書き timelineのページ用ファイル作成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>インスタントグラム</title>
-    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
     <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css">
     <link rel="stylesheet" type="text/css" href="css/app.css">
     <script src="https://dme0ih8comzn4.cloudfront.net/imaging/v2/editor.js"></script>
---
+++ resources/views/vendor/timeline.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>インスタントグラム</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
+    <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css">
+    <link rel="stylesheet" type="text/css" href="css/app.css">
+    <script src="https://dme0ih8comzn4.cloudfront.net/imaging/v2/editor.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+</head>
+<body>
+    <header class="header">
+        <div class="">instantgram</div>
+    </header>
+
+    <article class="timeline">
+        <p class="timeline__name">name name name</p>
+        <p class="timeline__img"><img src=""></p>
+        <p class="timeline__msg">テキストテキストテキスト</p>
+    </article>
+
+    <footer class="footer">
+        <ul class="footer__list">
+            <li class="is-selected"><p class="footer-list__inner"><img src="img/home.png" alt=""></p></li>
+            <li><p class="footer-list__inner"><img src="img/serch.png" alt=""></p></li>
+            <li><p class="footer-list__inner"><img src="img/upload.png" alt=""></p></li>
+            <li><p class="footer-list__inner"><img src="img/like.png" alt=""></p></li>
+            <li><p class="footer-list__inner"><img src="img/profile.png" alt=""></p></li>
+        </ul>
+    </footer>
+</body>
+</html>
